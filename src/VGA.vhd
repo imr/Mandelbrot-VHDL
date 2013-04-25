254ef43c3bcbbd517b81e6cfc99253f365f65d3a
Ian Roth <ian@hot-rain.com>
2013-04-25 12:23:01 -0400

    Update VGA code
    New signal to reset memory read address.

--- src/VGA.vhd
+++ src/VGA.vhd
@@ -8,7 +8,7 @@ USE ieee.numeric_std.all;
 
 ENTITY VGA IS
 	PORT(
-		VGA_CLK, VGA_BLANK_N, VGA_HS, VGA_VS :OUT STD_LOGIC;
+		VGA_CLK, VGA_BLANK_N, VGA_HS, VGA_VS, start :OUT STD_LOGIC;
 		clk, rst	:IN STD_LOGIC
 	);
 END ENTITY VGA;
@@ -16,6 +16,7 @@ END ENTITY VGA;
 ARCHITECTURE Behavior OF VGA IS
 	SIGNAL Hcount	:UNSIGNED(10 downto 0);
 	SIGNAL Vcount	:UNSIGNED(10 downto 0);
+	SIGNAL int_hs, int_vs :STD_LOGIC;
 	CONSTANT Hactive	:UNSIGNED(10 downto 0)	:= "10000000000"; -- 1024
 	CONSTANT Hsyncs	:UNSIGNED(10 downto 0)	:= "10000011001"; -- 1048 = 1024 + 24 (+1)
 	CONSTANT Hsynce	:UNSIGNED(10 downto 0)	:= "10010100001"; -- 1184 = 1024 + 24 + 136 (+1)
@@ -26,14 +27,17 @@ ARCHITECTURE Behavior OF VGA IS
 	CONSTANT Vtotal	:UNSIGNED(9 downto 0)	:= "1100100110"; -- 806
 BEGIN
 	VGA_CLK <= NOT clk;
+	start <= (NOT int_hs) AND (NOT int_vs);
+	VGA_HS <= int_hs;
+	VGA_VS <= int_vs;
 	PROCESS(clk, rst)
 	BEGIN
 		IF (clk'EVENT AND clk = '1') THEN
 			IF (rst = '1') THEN
 				Hcount <= "00000000000";
 				Vcount <= "00000000000";
-				VGA_HS <= '1';
-				VGA_VS <= '1';
+				int_hs <= '1';
+				int_vs <= '1';
 			ELSE
 				IF (Hcount < Htotal) THEN
 					Hcount <= Hcount + 1;
@@ -41,10 +45,10 @@ BEGIN
 					Hcount <= "00000000000";
 				END IF;
 				IF (Hcount = Hsyncs) THEN
-					VGA_HS <= '0';
+					int_hs <= '0';
 				END IF;
 				IF (Hcount = Hsynce) THEN
-					VGA_HS <= '1';
+					int_hs <= '1';
 					IF (Vcount < Vtotal) THEN
 						Vcount <= Vcount + 1;
 					ELSE
@@ -52,10 +56,10 @@ BEGIN
 					END IF;
 				END IF;
 				IF (Vcount = Vsysns) THEN
-					VGA_VS <= '0';
+					int_vs <= '0';
 				END IF;
 				IF (Vcount = Vsynce) THEN
-					VGA_VS <= '1';
+					int_vs <= '1';
 				END IF;
 				IF ((Hcount < Hactive) AND (Vcount < Vactive)) THEN
 					VGA_BLANK_N <= '1';
